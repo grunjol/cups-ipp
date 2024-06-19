30c8d2a749c94d56647cfbe96a37d0255f6d9b22
grunjol <grunjol@gmail.com>
2024-06-19 18:50:24 -0300

    change to guzzle psr client

--- src/Transport/Client.php
+++ src/Transport/Client.php
@@ -8,10 +8,11 @@ use Http\Client\Common\Plugin\ContentLengthPlugin;
 use Http\Client\Common\Plugin\DecoderPlugin;
 use Http\Client\Common\Plugin\ErrorPlugin;
 use Http\Client\Common\PluginClient;
-use Http\Client\HttpClient;
 use Http\Client\Socket\Client as SocketHttpClient;
 use Http\Message\MessageFactory\GuzzleMessageFactory;
 use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Client\ClientInterface;
 use Smalot\Cups\CupsException;
 
 /**
@@ -19,7 +20,7 @@ use Smalot\Cups\CupsException;
  *
  * @package Smalot\Cups\Transport
  */
-class Client implements HttpClient
+class Client implements ClientInterface
 {
 
     const SOCKET_URL = 'unix:///var/run/cups/cups.sock';
@@ -29,7 +30,7 @@ class Client implements HttpClient
     const AUTHTYPE_DIGEST = 'digest';
 
     /**
-     * @var HttpClient
+     * @var ClientInterface
      */
     protected $httpClient;
 
@@ -70,7 +71,7 @@ class Client implements HttpClient
         }
 
         $messageFactory = new GuzzleMessageFactory();
-        $socketClient = new SocketHttpClient($messageFactory, $socketClientOptions);
+        $socketClient = new SocketHttpClient($messageFactory, null, $socketClientOptions);
         $host = preg_match(
           '/unix:\/\//',
           $socketClientOptions['remote_socket']
@@ -116,7 +117,7 @@ class Client implements HttpClient
     /**
      * (@inheritdoc}
      */
-    public function sendRequest(RequestInterface $request)
+    public function sendRequest(RequestInterface $request): ResponseInterface
     {
         if ($this->username || $this->password) {
             switch ($this->authType) {
